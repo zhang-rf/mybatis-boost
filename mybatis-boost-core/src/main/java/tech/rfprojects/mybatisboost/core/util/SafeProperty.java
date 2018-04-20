4338fc028a5c84ebaf69450628a5333e3a0fd030
zhangrongfan <zhangrongfan@u51.com>
2018-04-20 11:26:10 +0800

    add util SafeProperty

---
+++ mybatis-boost-core/src/main/java/tech/rfprojects/mybatisboost/core/util/SafeProperty.java
@@ -0,0 +1,24 @@
+package tech.rfprojects.mybatisboost.core.util;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public abstract class SafeProperty {
+
+    private static ConcurrentMap<Integer, Boolean> cache = new ConcurrentHashMap<>();
+
+    public static String[] of(Class<?> type, String... properties) {
+        cache.computeIfAbsent(type.hashCode() ^ Arrays.hashCode(properties), (k) -> {
+            for (String property : properties) {
+                try {
+                    type.getDeclaredField(property);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return true;
+        });
+        return properties;
+    }
+}
