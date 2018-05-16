574670b533ecf24fe33fc760cb427aefd32d2a2c
zhangrongfan <zhangrongfan@u51.com>
2018-05-16 16:32:57 +0800

    modularize mapper

--- mybatis-boost-test/src/test/java/cn/mybatisboost/test/ProxyDogMapper.java
+++ mybatis-boost-test/src/test/java/cn/mybatisboost/test/ProxyDogMapper.java
@@ -1,8 +1,8 @@
 package cn.mybatisboost.test;
 
-import cn.mybatisboost.core.mapper.GenericMapper;
+import cn.mybatisboost.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface ProxyDogMapper extends GenericMapper<ProxyDog, Long> {
+public interface ProxyDogMapper extends BaseMapper<ProxyDog> {
 }
