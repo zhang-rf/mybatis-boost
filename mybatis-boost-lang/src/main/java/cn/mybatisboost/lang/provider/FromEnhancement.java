58138f7c1c91707673361d6a4f92fa2c9c73fac6
zhangrongfan <zhangrongfan@u51.com>
2018-05-24 16:21:17 +0800

    add FromEnhancement

---
+++ mybatis-boost-lang/src/main/java/cn/mybatisboost/lang/provider/FromEnhancement.java
@@ -0,0 +1,31 @@
+package cn.mybatisboost.lang.provider;
+
+import cn.mybatisboost.core.Configuration;
+import cn.mybatisboost.core.ConfigurationAware;
+import cn.mybatisboost.core.SqlProvider;
+import cn.mybatisboost.core.util.EntityUtils;
+import cn.mybatisboost.core.util.MapperUtils;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.reflection.MetaObject;
+
+public class FromEnhancement implements SqlProvider, ConfigurationAware {
+
+    private Configuration configuration;
+
+    @Override
+    public void replace(MetaObject metaObject, MappedStatement mappedStatement, BoundSql boundSql) {
+        String sql = boundSql.getSql();
+        if (sql.contains("#t")) {
+            Class<?> entityType = MapperUtils.getEntityTypeFromMapper
+                    (mappedStatement.getId().substring(0, mappedStatement.getId().lastIndexOf('.')));
+            metaObject.setValue("delegate.boundSql.sql", sql.replace("#t",
+                    EntityUtils.getTableName(entityType, configuration.getNameAdaptor())));
+        }
+    }
+
+    @Override
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+}
