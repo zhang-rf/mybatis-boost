c3de91c2b44b6c1feede27a2093eca76551f9dc2
zhangrongfan <zhangrongfan@u51.com>
2018-06-05 16:13:39 +0800

    fix UnsupportedOperationException

--- mybatis-boost-core/src/main/java/cn/mybatisboost/core/util/ParameterUtils.java
+++ mybatis-boost-core/src/main/java/cn/mybatisboost/core/util/ParameterUtils.java
@@ -6,38 +6,34 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public abstract class ParameterUtils {
 
-    private static ConcurrentMap<MultipleMapKey, Map<String, Object>> parameterMapCache = new ConcurrentHashMap<>();
-
     public static Map<String, Object> buildParameterMap(List<String> properties, Class<?> type, List<?> entities) {
-        return parameterMapCache.computeIfAbsent(new MultipleMapKey(type, properties), k -> {
-            Map<String, Object> parameterMap = new HashMap<>(properties.size() * entities.size());
-            List<Method> readMethods = new ArrayList<>();
+        Map<String, Object> parameterMap = new HashMap<>(properties.size() * entities.size());
+        List<Method> readMethods = new ArrayList<>();
 
+        try {
+            PropertyDescriptor[] descriptors = Introspector.getBeanInfo(type).getPropertyDescriptors();
+            for (String property : properties) {
+                readMethods.add(Arrays.stream(descriptors).filter(d -> Objects.equals(d.getName(), property))
+                        .findAny().orElseThrow(NoSuchFieldError::new).getReadMethod());
+            }
+        } catch (IntrospectionException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < entities.size(); i++) {
             try {
-                PropertyDescriptor[] descriptors = Introspector.getBeanInfo(type).getPropertyDescriptors();
-                for (String property : properties) {
-                    readMethods.add(Arrays.stream(descriptors).filter(d -> Objects.equals(d.getName(), property))
-                            .findAny().orElseThrow(NoSuchFieldError::new).getReadMethod());
+                for (int n = 0; n < readMethods.size(); n++) {
+                    parameterMap.put(properties.get(n) + i, readMethods.get(n).invoke(entities.get(i)));
                 }
-            } catch (IntrospectionException e) {
+            } catch (IllegalAccessException | InvocationTargetException e) {
                 throw new RuntimeException(e);
             }
+        }
 
-            for (int i = 0; i < entities.size(); i++) {
-                try {
-                    for (int n = 0; n < readMethods.size(); n++) {
-                        parameterMap.put(properties.get(n) + i, readMethods.get(n).invoke(entities.get(i)));
-                    }
-                } catch (IllegalAccessException | InvocationTargetException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return Collections.unmodifiableMap(parameterMap);
-        });
+        parameterMap.put("list", entities);
+        return parameterMap;
     }
 }
