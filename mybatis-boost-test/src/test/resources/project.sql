490f36b6605a30df25d9befc548a2850dbb032f9
zhangrongfan <zhangrongfan@u51.com>
2018-06-28 16:48:58 +0800

    fix sql
    fix sql

--- mybatis-boost-test/src/test/resources/project.sql
+++ mybatis-boost-test/src/test/resources/project.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS test;
-CREATE TABLE `test`.`Project`
+CREATE TABLE `test`.`project`
 (
   id                 int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   group_id           varchar(100)    NOT NULL,
