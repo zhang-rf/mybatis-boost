7fcde21f1ced6276c99bac295781e6230bcc5316
zhangrongfan <zhangrongfan@u51.com>
2018-06-29 17:39:29 +0800

    add CrudMapper unit test

--- mybatis-boost-test/src/test/java/cn/mybatisboost/test/CrudMapperTest.java
+++ mybatis-boost-test/src/test/java/cn/mybatisboost/test/CrudMapperTest.java
@@ -1,6 +1,7 @@
 package cn.mybatisboost.test;
 
 import cn.mybatisboost.core.util.SafeProperty;
+import org.apache.ibatis.session.RowBounds;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootApplication
@@ -54,69 +56,165 @@ public class CrudMapperTest {
 
     @Test
     public void select() {
+        assertTrue(mapper.select(new Project()).isEmpty());
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost')");
+        assertEquals("cn.mybatisboost",
+                mapper.select(new Project().setGroupId("cn.mybatisboost")).get(0).getGroupId());
+        assertTrue(mapper.select(new Project().setGroupId("whatever")).isEmpty());
     }
 
     @Test
     public void selectWithRowBounds() {
+        assertTrue(mapper.selectWithRowBounds(new Project(), RowBounds.DEFAULT).isEmpty());
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost1')");
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost2')");
+        assertEquals(2, mapper.selectWithRowBounds(new Project(), RowBounds.DEFAULT).size());
+        assertEquals(1, mapper.selectWithRowBounds(new Project(), new RowBounds(1, 1)).size());
+        assertEquals("cn.mybatisboost2",
+                mapper.selectWithRowBounds(new Project(), new RowBounds(1, 1)).get(0).getGroupId());
     }
 
     @Test
     public void countAll() {
+        assertEquals(0, mapper.countAll());
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost')");
+        assertEquals(1, mapper.countAll());
     }
 
     @Test
     public void selectAll() {
+        assertEquals(0, mapper.selectAll().size());
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost')");
+        assertEquals(1, mapper.selectAll().size());
     }
 
     @Test
     public void selectAllWithRowBounds() {
+        assertTrue(mapper.selectAllWithRowBounds(RowBounds.DEFAULT).isEmpty());
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost1')");
+        jdbcTemplate.execute("insert into project (group_id) values ('cn.mybatisboost2')");
+        assertEquals(2, mapper.selectAllWithRowBounds(RowBounds.DEFAULT).size());
+        assertEquals(1, mapper.selectAllWithRowBounds(new RowBounds(1, 1)).size());
+        assertEquals("cn.mybatisboost2",
+                mapper.selectAllWithRowBounds(new RowBounds(1, 1)).get(0).getGroupId());
     }
 
     @Test
     public void selectById() {
+        assertNull(mapper.selectById(123));
+        jdbcTemplate.execute("insert into project (id, group_id) values (123, 'cn.mybatisboost')");
+        assertEquals(123, mapper.selectById(123).getId().intValue());
     }
 
     @Test
     public void selectByIds() {
+        assertTrue(mapper.selectByIds(123).isEmpty());
+        jdbcTemplate.execute("insert into project (id, group_id) values (123, 'cn.mybatisboost1')");
+        jdbcTemplate.execute("insert into project (id, group_id) values (456, 'cn.mybatisboost2')");
+        assertEquals(2, mapper.selectByIds(123, 456).size());
+        assertEquals(123, mapper.selectByIds(123, 456).get(0).getId().intValue());
+        assertEquals(456, mapper.selectByIds(123, 456).get(1).getId().intValue());
     }
 
     @Test
     public void insert() {
+        try {
+            assertEquals(1, mapper.insert(new Project("cn.mybatisboost", "mybatis-boost",
+                    "MIT", "https://github.com/zhang-rf/mybatis-boost", "zhangrongfan")));
+            fail();
+        } catch (Exception ignored) {
+        }
     }
 
     @Test
     public void batchInsert() {
+        try {
+            assertEquals(1, mapper.batchInsert(Collections.singletonList(
+                    new Project("cn.mybatisboost", "mybatis-boost",
+                            "MIT", "https://github.com/zhang-rf/mybatis-boost", "zhangrongfan"))));
+            fail();
+        } catch (Exception ignored) {
+        }
     }
 
     @Test
     public void insertSelectively() {
+        assertEquals(1, mapper.insertSelectively(new Project("cn.mybatisboost", "mybatis-boost",
+                "MIT", "https://github.com/zhang-rf/mybatis-boost", "zhangrongfan")));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+        });
     }
 
     @Test
     public void batchInsertSelectively() {
+        assertEquals(1, mapper.batchInsertSelectively(Collections.singletonList(
+                new Project("cn.mybatisboost", "mybatis-boost",
+                        "MIT", "https://github.com/zhang-rf/mybatis-boost", "zhangrongfan"))));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+        });
     }
 
     @Test
     public void update() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        assertEquals(1, mapper.update(new Project().setId(123).setGroupId("cn.mybatisboost2")));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+            assertEquals("cn.mybatisboost2", resultSet.getString("group_id"));
+            assertNull(resultSet.getString("artifact_id"));
+        });
     }
 
     @Test
     public void updatePartially() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        assertEquals(1, mapper.updatePartially(new Project().setId(123).setGroupId("cn.mybatisboost2"),
+                SafeProperty.of(Project.class, "groupId")));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+            assertEquals("cn.mybatisboost2", resultSet.getString("group_id"));
+            assertEquals("mybatis-boost", resultSet.getString("artifact_id"));
+        });
     }
 
     @Test
     public void updateSelectively() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        assertEquals(1, mapper.updateSelectively(new Project().setId(123).setGroupId("cn.mybatisboost2")));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+            assertEquals("cn.mybatisboost2", resultSet.getString("group_id"));
+            assertEquals("mybatis-boost", resultSet.getString("artifact_id"));
+        });
     }
 
     @Test
     public void updatePartiallySelectively() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        assertEquals(1, mapper.updateSelectively(new Project().setId(123).setGroupId("cn.mybatisboost2"),
+                SafeProperty.of(Project.class, "groupId")));
+        jdbcTemplate.query("select * from project", resultSet -> {
+            assertEquals(1, resultSet.getRow());
+            assertEquals("cn.mybatisboost2", resultSet.getString("group_id"));
+            assertEquals("mybatis-boost", resultSet.getString("artifact_id"));
+        });
     }
 
     @Test
     public void delete() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (456, 'cn.mybatisboost2', 'mybatis-boost')");
+        assertEquals(0, mapper.delete(new Project()));
+        assertEquals(2, mapper.delete(new Project().setArtifactId("mybatis-boost")));
     }
 
     @Test
     public void deleteByIds() {
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (123, 'cn.mybatisboost1', 'mybatis-boost')");
+        jdbcTemplate.execute("insert into project (id, group_id, artifact_id) values (456, 'cn.mybatisboost2', 'mybatis-boost')");
+        assertEquals(0, mapper.deleteByIds(0));
+        assertEquals(2, mapper.deleteByIds(123, 456));
     }
 }
