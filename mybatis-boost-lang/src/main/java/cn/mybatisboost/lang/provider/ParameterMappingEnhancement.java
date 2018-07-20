1aeb4bf493298c4476c381bae89b1e3e700995ea
zhangrongfan <zhangrongfan@u51.com>
2018-07-20 16:30:30 +0800

    simplify

--- mybatis-boost-lang/src/main/java/cn/mybatisboost/lang/provider/ParameterMappingEnhancement.java
+++ mybatis-boost-lang/src/main/java/cn/mybatisboost/lang/provider/ParameterMappingEnhancement.java
@@ -7,7 +7,6 @@ import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,13 +18,11 @@ public class ParameterMappingEnhancement implements SqlProvider {
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
         if (parameterMappings.isEmpty() && parameterObject instanceof Map) {
             Map<?, ?> parameterMap = (Map<?, ?>) parameterObject;
-            parameterMappings = new ArrayList<>(parameterMap.size() / 2);
             Configuration configuration = (Configuration) metaObject.getValue("delegate.configuration");
             for (int i = 1; parameterMap.containsKey("param" + i); i++) {
                 parameterMappings.add(new ParameterMapping.Builder
                         (configuration, "param" + i, Object.class).build());
             }
-            metaObject.setValue("delegate.boundSql.parameterMappings", parameterMappings);
         }
     }
 }
