f1297ca8510a4a861eca069225a52218fb54a87a
zhangrongfan <zhangrongfan@u51.com>
2018-10-19 17:15:00 +0800

    Refactor and introduce "nosql"

--- mybatis-boost-core/src/main/java/cn/mybatisboost/nosql/Predicate.java
+++ mybatis-boost-core/src/main/java/cn/mybatisboost/nosql/Predicate.java
@@ -5,7 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Predicate {
-    And("And", "AND", false), Or("Or", "OR", false), Between("Bw", "BETWEEN ? AND ?", true), Lessthan("Lt", "< ?", true), Lessthanequal("Lte", "<= ?", true), Greaterthan("Gt", "> ?", true), Greaterthanequal("Gte", ">= ?", true), Isnull("Null", "IS NULL", true), Isempty("Empty", "= ''", true), Like("Like", "LIKE ?", true), Orderby("Ob", "ORDER BY", false), Not("Not", "NOT", false), In("In", "IN ?", true), True("True", "= TRUE", true), False("False", "= FALSE", true), Asc("Asc", "ASC", true), Desc("Desc", "DESC", true);
+    And("And", "AND", false),
+    Or("Or", "OR", false),
+    Between("Bw", "BETWEEN ? AND ?", true),
+    Lessthan("Lt", "< ?", true),
+    Lessthanequal("Lte", "<= ?", true),
+    Greaterthan("Gt", "> ?", true),
+    Greaterthanequal("Gte", ">= ?", true),
+    Isnull("Null", "IS NULL", true),
+    Isempty("Empty", "= ''", true),
+    Like("Like", "LIKE ?", true),
+    Orderby("Ob", "ORDER BY", false),
+    Not("Not", "NOT", false),
+    In("In", "IN ?", true),
+    True("True", "= TRUE", true),
+    False("False", "= FALSE", true),
+    Asc("Asc", "ASC", true),
+    Desc("Desc", "DESC", true);
 
     private static Map<String, Predicate> aliasMap = new HashMap<>();
     private String alias, sqlFragment;
