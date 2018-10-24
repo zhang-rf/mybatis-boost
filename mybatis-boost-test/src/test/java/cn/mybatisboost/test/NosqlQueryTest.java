ed8a8cac537848e068972166ff065ef8fe5bcdfd
zhangrongfan <zhangrongfan@u51.com>
2018-10-24 15:00:38 +0800

    Tests

--- mybatis-boost-test/src/test/java/cn/mybatisboost/test/NosqlQueryTest.java
+++ mybatis-boost-test/src/test/java/cn/mybatisboost/test/NosqlQueryTest.java
@@ -4,7 +4,6 @@ import cn.mybatisboost.core.GenericMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,11 +14,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class NosqlQueryTest {
 
     @Autowired
-    @Qualifier("projectNosqlMapper")
-    private GenericMapper<Project> mapper;
+    private ProjectNosqlMapper mapper;
 
     @Test
     public void test() throws Exception {
-        System.out.println(mapper.getClass().getMethod("selectFirst").invoke(mapper));
+        System.out.println(mapper.selectFirst());
     }
 }
