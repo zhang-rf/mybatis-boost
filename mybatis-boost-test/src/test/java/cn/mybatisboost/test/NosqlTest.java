90135838db43aa082a3fbf999d125c02f7ebed99
zhangrongfan <zhangrongfan@u51.com>
2018-11-16 15:50:06 +0800

    2.1.0 PRE-RELEASE

--- mybatis-boost-test/src/test/java/cn/mybatisboost/test/NosqlTest.java
+++ mybatis-boost-test/src/test/java/cn/mybatisboost/test/NosqlTest.java
@@ -1,22 +1,35 @@
 package cn.mybatisboost.test;
 
+import cn.mybatisboost.core.GenericMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * We use reflection to test Nosql feature
+ * mainly because we mustn't but have to load mapper classes before there were scanned by Mybatis
+ */
 @RunWith(SpringRunner.class)
+@SpringBootApplication
+@SpringBootTest(classes = GenericMapper.class)
+@SuppressWarnings("unchecked")
 public class NosqlTest {
 
     @Autowired
-    private ProjectNosqlMapper mapper;
+    @Qualifier("projectNosqlMapper")
+    private GenericMapper mapper;
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -33,55 +46,63 @@ public class NosqlTest {
     }
 
     @Test
-    public void deleteAll() {
-        assertEquals(3, mapper.deleteAll());
+    public void deleteAll() throws Exception {
+        Method deleteAll = mapper.getClass().getDeclaredMethod("deleteAll");
+        assertEquals(3, deleteAll.invoke(mapper));
         jdbcTemplate.query("select * from project", resultSet -> {
             assertEquals(0, resultSet.getRow());
         });
     }
 
     @Test
-    public void selectFirst() {
-        assertEquals(123, (int) mapper.selectFirst().getId());
+    public void selectFirst() throws Exception {
+        Method selectFirst = mapper.getClass().getDeclaredMethod("selectFirst");
+        assertEquals(123, (int) ((Project) selectFirst.invoke(mapper)).getId());
     }
 
     @Test
-    public void selectTop2() {
-        List<Project> list = mapper.selectTop2();
+    public void selectTop2() throws Exception {
+        Method selectTop2 = mapper.getClass().getDeclaredMethod("selectTop2");
+        List<Project> list = (List<Project>) selectTop2.invoke(mapper);
         assertEquals(2, list.size());
         assertEquals(123, (int) list.get(0).getId());
         assertEquals(456, (int) list.get(1).getId());
     }
 
     @Test
-    public void selectAllOffset1Limit1() {
-        assertEquals(456, (int) mapper.selectAllOffset1Limit1().getId());
+    public void selectAllOffset1Limit1() throws Exception {
+        Method selectAllOffset1Limit1 = mapper.getClass().getDeclaredMethod("selectAllOffset1Limit1");
+        assertEquals(456, (int) ((Project) selectAllOffset1Limit1.invoke(mapper)).getId());
     }
 
     @Test
-    public void selectByGroupIdAndArtifactId() {
-        assertEquals(123, (int) mapper.selectByGroupIdAndArtifactId("cn.mybatisboost1", "mybatis-boost1").get(0).getId());
+    public void selectByGroupIdAndArtifactId() throws Exception {
+        Method selectByGroupIdAndArtifactId = mapper.getClass().getDeclaredMethod("selectByGroupIdAndArtifactId", String.class, String.class);
+        assertEquals(123, (int) ((Project) selectByGroupIdAndArtifactId.invoke(mapper, "cn.mybatisboost1", "mybatis-boost1")).getId());
     }
 
     @Test
-    public void selectByGroupIdOrArtifactId() {
-        List<Project> list = mapper.selectByGroupIdOrArtifactId("cn.mybatisboost1", "mybatis-boost2");
+    public void selectByGroupIdOrArtifactId() throws Exception {
+        Method selectByGroupIdOrArtifactId = mapper.getClass().getDeclaredMethod("selectByGroupIdOrArtifactId", String.class, String.class);
+        List<Project> list = (List<Project>) selectByGroupIdOrArtifactId.invoke(mapper, "cn.mybatisboost1", "mybatis-boost2");
         assertEquals(2, list.size());
         assertEquals(123, (int) list.get(0).getId());
         assertEquals(456, (int) list.get(1).getId());
     }
 
     @Test
-    public void selectByNotArtifactId() {
-        List<Project> list = mapper.selectByArtifactIdNot("mybatis-boost1");
+    public void selectByNotArtifactId() throws Exception {
+        Method selectByArtifactIdNot = mapper.getClass().getDeclaredMethod("selectByArtifactIdNot", String.class);
+        List<Project> list = (List<Project>) selectByArtifactIdNot.invoke(mapper, "mybatis-boost1");
         assertEquals(2, list.size());
         assertEquals(456, (int) list.get(0).getId());
         assertEquals(789, (int) list.get(1).getId());
     }
 
     @Test
-    public void selectAllOrderByGroupIdDesc() {
-        List<Project> list = mapper.selectAllOrderByGroupIdDesc();
+    public void selectAllOrderByGroupIdDesc() throws Exception {
+        Method selectAllOrderByGroupIdDesc = mapper.getClass().getDeclaredMethod("selectAllOrderByGroupIdDesc");
+        List<Project> list = (List<Project>) selectAllOrderByGroupIdDesc.invoke(mapper);
         assertEquals(3, list.size());
         assertEquals(789, (int) list.get(0).getId());
         assertEquals(456, (int) list.get(1).getId());
