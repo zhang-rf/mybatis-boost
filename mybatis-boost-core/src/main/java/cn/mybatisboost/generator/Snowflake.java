6d0941a0b9fd178a4acbb455d929697d64db5eda
zhangrongfan <zhangrongfan@u51.com>
2018-12-26 20:05:46 +0800

    Introduce Snowflake id generator

---
+++ mybatis-boost-core/src/main/java/cn/mybatisboost/generator/Snowflake.java
@@ -0,0 +1,51 @@
+package cn.mybatisboost.generator;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class Snowflake {
+
+    private long epoch;
+    private long identity;
+    private int timestampShifting;
+    private int identityShifting;
+    private long maxSequence;
+
+    private long lastTimestamp;
+    private ArrayBlockingQueue<Long> idQueue;
+
+    public Snowflake(long epoch, int identity) {
+        this(epoch, identity, 10, 12);
+    }
+
+    public Snowflake(long epoch, int identity, int identityBits, int sequenceBits) {
+        this.epoch = epoch;
+        this.identity = identity;
+        timestampShifting = sequenceBits + identityBits;
+        identityShifting = sequenceBits;
+        maxSequence = ~(-1L << sequenceBits);
+        idQueue = new ArrayBlockingQueue<>((int) maxSequence + 1);
+    }
+
+    public long next() throws InterruptedException {
+        long currentTimeMillis = System.currentTimeMillis() - epoch;
+        if (currentTimeMillis != lastTimestamp) {
+            synchronized (this) {
+                if (currentTimeMillis != lastTimestamp) {
+                    if (currentTimeMillis < lastTimestamp) {
+                        throw new IllegalStateException("Clock moved backwards");
+                    }
+                    idQueue.clear();
+                    long id = currentTimeMillis << timestampShifting;
+                    id |= identity << identityShifting;
+                    for (int i = 0; i <= maxSequence; i++) {
+                        idQueue.offer(id | i);
+                    }
+                    lastTimestamp = currentTimeMillis;
+                }
+            }
+        }
+        Long next = idQueue.poll(1, TimeUnit.MILLISECONDS);
+        return next == null ? next() : next;
+    }
+}
