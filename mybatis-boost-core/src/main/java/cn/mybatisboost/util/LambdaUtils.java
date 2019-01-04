1f1461e82865be58707dd55621515e8f76f01aae
zhangrongfan <zhangrongfan@u51.com>
2019-01-04 17:07:55 +0800

    Add LambdaUtils

---
+++ mybatis-boost-core/src/main/java/cn/mybatisboost/util/LambdaUtils.java
@@ -0,0 +1,87 @@
+package cn.mybatisboost.util;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class LambdaUtils implements MethodInterceptor {
+
+    private static Pattern CLASS_CAST_PATTERN = Pattern.compile("cannot be cast to (.+)$");
+    private LambdaInfo lambdaInfo;
+
+    @SuppressWarnings("unchecked")
+    public static <T, R> LambdaInfo getLambdaInfo(Function<T, R> function) {
+        Enhancer enhancer = new Enhancer();
+        LambdaUtils that = new LambdaUtils() {
+        };
+        enhancer.setCallback(that);
+        try {
+            function.apply((T) enhancer.create());
+        } catch (ClassCastException e) {
+            Matcher matcher = CLASS_CAST_PATTERN.matcher(e.getMessage());
+            if (matcher.find()) {
+                try {
+                    Class<?> type = function.getClass().getClassLoader().loadClass(matcher.group(1));
+                    enhancer.setSuperclass(type);
+                    function.apply((T) enhancer.create());
+                } catch (ClassNotFoundException e1) {
+                    throw new RuntimeException(e1);
+                }
+            }
+        }
+        return that.lambdaInfo;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) {
+        this.lambdaInfo = new LambdaInfo(method.getDeclaringClass(), method.getName());
+        return null;
+    }
+
+    public static class LambdaInfo {
+
+        private Class<?> type;
+        private String methodName;
+
+        public LambdaInfo(Class<?> type, String methodName) {
+            this.type = type;
+            this.methodName = methodName;
+        }
+
+        public Class<?> getType() {
+            return type;
+        }
+
+        public String getMethodName() {
+            return methodName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            LambdaInfo that = (LambdaInfo) o;
+            return type.equals(that.type) &&
+                    methodName.equals(that.methodName);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(type, methodName);
+        }
+
+        @Override
+        public String toString() {
+            return "LambdaInfo{" +
+                    "type=" + type +
+                    ", methodName='" + methodName + '\'' +
+                    '}';
+        }
+    }
+}
